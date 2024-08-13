b60c9e19f9b949357b4713ab3a6f8bb65f154999
Hoang Btmrk <hoang@bitmark.com>
2024-08-13 16:52:45 +0700

    fix lint

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -14,7 +14,7 @@ export class KeyEvent extends PlatformEventReceiver {
     super.handlePlatformEvent(event);
     const [keyId, keyLabel] = event.split('_');
     console.log(`Handling key event: ${keyId} - ${keyLabel}`);
-    if (parseInt(keyId) !== KeyCodes.escape) {
+    if (keyId !== KeyCodes.escape.toString()) {
       EventEmitter.emit(Event.keyDown);
     }
   }
