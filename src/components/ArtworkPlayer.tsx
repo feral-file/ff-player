d05c5aba1fb9c31c642ef252fa4a07b5a2ba45c0
anhthuqs99 <anhthuptqs99@gmail.com>
2024-08-23 17:14:08 +0700

    fix loading size indicator

--- src/components/ArtworkPlayer.tsx
+++ src/components/ArtworkPlayer.tsx
@@ -1,4 +1,5 @@
 import { LocalStorageItem } from '@/constants';
+import { AppContext } from '@/context/AppContext';
 import mixpanel, {
   CastArtworkEventProperties,
   CastingArtworkType,
@@ -23,7 +24,7 @@ import {
 } from '@/utils/types';
 import Hls from 'hls.js';
 import Image from 'next/image';
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 
 const ArtworkPlayer = ({
   previewURL,
@@ -37,6 +38,10 @@ const ArtworkPlayer = ({
   castingType?: CastingArtworkType;
   keyboardCode?: number;
 }) => {
+  const context = useContext(AppContext);
+  const { screenRatio } = context?.deviceRotation ?? {
+    screenRatio: 1,
+  };
   const [previewType, setPreviewType] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -217,12 +222,18 @@ const ArtworkPlayer = ({
               }}>
               <Image
                 src="/ff-loading-still-v2.svg"
-                width={430}
-                height={288}
+                width={430 * screenRatio}
+                height={288 * screenRatio}
                 objectFit="contain"
                 alt="Loading"></Image>
             </div>
-            <p>Loading</p>
+            <p
+              style={{
+                fontSize: screenRatio * 28,
+                marginTop: 16 * screenRatio,
+              }}>
+              Loading
+            </p>
           </div>
         </div>
       )}
