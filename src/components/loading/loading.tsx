ace361e6953600a4cccb6cf4e3b7b4a2da4763f7
Kien Nguyen Trung <kien@bitmark.com>
2024-08-28 21:55:30 +0700

    update loading

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -3,7 +3,8 @@ import styles from './styles.module.scss';
 export default function Loading() {
   return (
     <div className={styles.loading}>
-      <div className={styles['loading-item ']}>Loading ...</div>
+      <div>Loading ...</div>
+      <div className={styles.loader}></div>
     </div>
   );
 }
