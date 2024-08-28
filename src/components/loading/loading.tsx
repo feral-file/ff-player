2130316cbe33239246fbe7225d3f52fa0bd51c1a
anhthuqs99 <anhthuptqs99@gmail.com>
2024-08-28 11:41:07 +0700

    add loading text

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 export default function Loading() {
   return (
     <div className={styles.loading}>
-      <div className={styles['loading-item ']}></div>
+      <div className={styles['loading-item ']}>Loading ...</div>
     </div>
   );
 }
