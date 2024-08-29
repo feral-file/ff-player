209acef74a5561b90c5b8c2957351d926e1a7583
Kien Nguyen Trung <kien@bitmark.com>
2024-08-29 09:06:26 +0700

    remove space

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
     <div className={styles.loading}>
       <div className={styles.loadingContainer}>
         <div>
-          <div>Loading ...</div>
+          <div>Loading...</div>
           <div className={styles.loader}></div>
         </div>
       </div>
