4097d13125f4e50c6045f73e603057e46fa8bc51
Kien Nguyen Trung <kien@bitmark.com>
2024-09-04 10:51:26 +0700

    update version

--- src/constants.ts
+++ src/constants.ts
@@ -67,7 +67,7 @@ export enum DeviceNamePrefix {
 }
 
 export const AppSettings = {
-  VERSION_CHECK_INTERVAL_DURATION: 1000 * 60 * 60, // 1 minutes
+  VERSION_CHECK_INTERVAL_DURATION: 1000 * 60, // 1 minutes
   JG_043_EXHIBITION_ID: '46a0f68b-a657-4364-92a0-32a88b65fbd9',
   STANDARD_HEIGHT: 1080,
 };
