0a152f24cc83b56b74de42b89a2e0bdee6a832fe
Kien Nguyen Trung <kien@bitmark.com>
2024-09-16 10:38:58 +0700

    play next exh

--- src/app/daily/daily-client.tsx
+++ src/app/daily/daily-client.tsx
@@ -28,7 +28,44 @@ export default function DailyClient() {
   const [isLeeMucianExhibition, setIsLeeMucianExhibition] =
     useState<boolean>(false);
 
+  const [index, setIndex] = useState<number>(0);
+  const previewURLs = [
+    'https://cdn.feralfileassets.com/previews/4e5e1cc5-3038-443b-8f5b-f35cab89811d/1721897970/_unique-previews/0',
+    'https://cdn.feralfileassets.com/previews/5f673d60-a2c0-4356-8bf9-09dc0d43f44b/1721897970/_unique-previews/0',
+    'https://cdn.feralfileassets.com/previews/4fbf3d62-4824-4e2a-9f5c-8051f7c7d566/1724033663/_unique-previews/0',
+    'https://cdn.feralfileassets.com/previews/dce8e5ee-869d-41f6-8c0b-de98a4a0a98f/1726130590/index.html?edition_number=0&artwork_number=1&blockchain=ethereum&token_id=1348537551383340122739544414467967762976016640&token_id_hash=0x72b47c1a336bc8b86afa341cf46ec920eec2c8b083d6b4076fa5822d8c3e1ec2',
+    'https://cdn.feralfileassets.com/previews/ab6684c6-c8bd-474e-937c-55ca68591085/1724337315/index.html?edition_number=0&artwork_number=1&blockchain=ethereum&token_id=1348537551383340122739544414467967762977016640&token_id_hash=0x03f0d49d83f15cd8eb7f036a03332a670c9513895c13ee9c893be086f0bc2f3d',
+    'https://cdn.feralfileassets.com/previews/4d23d1ce-97c8-411b-b615-e64ca8d78c3c/1724327523/index.html?edition_number=0&artwork_number=1&blockchain=ethereum&token_id=1348537551383340122739544414467967762978016640&token_id_hash=0x3c49b838cae0f2bd69e5db22e86e966d378c182b60c5bae81145afaea8520d3b',
+    'https://cdn.feralfileassets.com/previews/3ad37701-9935-438e-8a9c-9dcadab429a6/1724241986/index.html?edition_number=0&artwork_number=1&blockchain=ethereum&token_id=1348537551383340122739544414467967762979016640&token_id_hash=0xd792353df80b14bd2b47bdc59882a29d96218ec0021f8bfe92efe4a3daa1a3f7',
+    'https://cdn.feralfileassets.com/previews/87bf9fc7-cab0-4a60-ac73-6a210ddbbcbc/1724033852/_unique-previews/0',
+    'https://cdn.feralfileassets.com/previews/ccd387c2-4762-4037-8752-bbc223957199/1724764457/index.html?edition_number=0&artwork_number=1&blockchain=ethereum&token_id=1348537551383340122739544414467967762981016640&token_id_hash=0x0d9ea64e929000e71e02adcb40bacd55da8acc21f7d804604cfa8b896983edd5',
+    'https://cdn.feralfileassets.com/previews/a62734e6-bce4-49a6-ab10-1b9698085efa/1725388435/index.html?edition_number=0&artwork_number=1&blockchain=ethereum&token_id=1348537551383340122739544414467967762982016640&token_id_hash=0x83012ee9576e78df7c8a3b5db81d78bdd1d40b5a7d486178945e9420e6466d49',
+  ];
+
   useEffect(() => {
+    const previewURL = previewURLs[index];
+    if (previewURL) {
+      setCastPreviewURL(previewURL);
+    }
+  }, [index]);
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setIndex(prevIndex => {
+        if (prevIndex >= previewURLs.length - 1) {
+          return 0;
+        }
+        return prevIndex + 1;
+      });
+    }, 60 * 1000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, [previewURLs.length]);
+
+  useEffect(() => {
+    return;
+
     if (typeof window !== 'undefined') {
       try {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
