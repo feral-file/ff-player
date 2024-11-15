99b0674f75e19cc2d6e4a497f245b0da3e20ebc3
PuPha <nguyensanghso@gmail.com>
2024-11-15 14:44:11 +0700

    use DASH

--- src/services/series.service.ts
+++ src/services/series.service.ts
@@ -59,8 +59,9 @@ export class SeriesService {
 
   public getArtworkPreview(artwork: Artwork): string {
     const previewUrl =
-      (artwork.previewDisplay?.['HLS']
-           ? artwork.previewDisplay['HLS'] + '?clientBandwidthHint=1.8' : null) ??
+      (artwork.previewDisplay?.DASH
+        ? artwork.previewDisplay!.DASH + '?clientBandwidthHint=1.8'
+        : null) ??
       artwork.metadata?.alternativePreviewURI ??
       artwork.metadata?.previewCloudFlareURL ??
       artwork.previewURI;
