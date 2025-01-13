b177f350d5036779981a0e95532842c9eb998767
Hoang Btmrk <hoang@bitmark.com>
2025-01-13 11:15:12 +0700

    feat: add FFDeviceRequestEvent platform function

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -23,7 +23,12 @@ import RemoteConfigService, {
 } from '@/services/remoteConfigService';
 import { AppSettings, LocalStorageItem, Platform } from '@/constants';
 import { useSearchParams } from 'next/navigation';
-import { Config, DeviceName, KeyEvent } from '@/utils/platform';
+import {
+  Config,
+  DeviceName,
+  FeralfileDeviceEvent,
+  KeyEvent,
+} from '@/utils/platform';
 import DeviceManager from '@/utils/DeviceManager';
 import useLoadScript from '@/services/LoadScripts';
 import useAppControls, {
@@ -184,6 +189,11 @@ export const AppProvider = ({ children }: AppContextProps) => {
       (window as any).Config = {
         // eslint-disable-next-line @typescript-eslint/unbound-method
         handlePlatformEvent: Config.handlePlatformEvent,
+      };
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+      (window as any).FeralfileDevice = {
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        requestCommand: FeralfileDeviceEvent.requestCommand,
       };
 
       const pl = searchParams?.get('platform') ?? '';
