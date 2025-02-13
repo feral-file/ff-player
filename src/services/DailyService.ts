96245e560c0a7bda72d3ba757ffe1c30e176c7a5
tsthien <anewday1999@gmail.com>
2025-02-13 16:47:26 +0700

    Test

--- src/services/DailyService.ts
+++ src/services/DailyService.ts
@@ -212,7 +212,7 @@ class DailyService {
     // Format date to yyyy-mm-dd
     const year = date.getFullYear().toString();
     const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+    const day = String(date.getDate() - 2).padStart(2, '0');
     return `${year}-${month}-${day}`;
   }
 }
