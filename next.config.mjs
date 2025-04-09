e13019ddfe3e5bc4f76c905414f790bb437c2eec
Hoang Btmrk <hoang@bitmark.com>
2025-04-09 16:13:16 +0700

    fix: remove localization

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
   output: 'export',
@@ -66,7 +63,7 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(withNextIntl(nextConfig), {
+export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
