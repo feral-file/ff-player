00b2c074115b37e4c2ed8f86bf2064a0800a00fb
Hoang Btmrk <hoang@bitmark.com>
2025-05-23 14:41:17 +0700

    refactor: handle axios, apollo client error catched

--- src/services/axiosService.ts
+++ src/services/axiosService.ts
@@ -1,4 +1,24 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+
+// Error handler for axios instances
+const handleAxiosError = (error: AxiosError) => {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.error('[Axios Error] Response:', {
+      status: error.response.status,
+      data: error.response.data,
+      headers: error.response.headers,
+    });
+  } else if (error.request) {
+    // The request was made but no response was received
+    console.error('[Axios Error] Request:', error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.error('[Axios Error] Message:', error.message);
+  }
+  return Promise.reject(error);
+};
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -7,6 +27,9 @@ const axiosInstance = axios.create({
   },
 });
 
+// Add error interceptor to main axios instance
+axiosInstance.interceptors.response.use(response => response, handleAxiosError);
+
 export const supportAxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SUPPORT_API_URL,
   headers: {
@@ -14,5 +37,11 @@ export const supportAxiosInstance = axios.create({
     'x-api-key': process.env.NEXT_PUBLIC_SUPPORT_API_KEY,
   },
 });
+
+// Add error interceptor to support axios instance
+supportAxiosInstance.interceptors.response.use(
+  response => response,
+  handleAxiosError
+);
 
 export default axiosInstance;
