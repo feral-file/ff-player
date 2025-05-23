75fcbfea6e7ec9b6dda25323d028f94e411d9f8b
Hoang Btmrk <hoang@bitmark.com>
2025-05-23 14:41:17 +0700

    refactor: handle axios, apollo client error catched

--- src/utils/ApolloClient.ts
+++ src/utils/ApolloClient.ts
@@ -1,9 +1,49 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, from, HttpLink } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { GraphQLFormattedError } from 'graphql';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(
+      ({ message, locations, path }: GraphQLFormattedError) => {
+        const locationStr =
+          locations
+            ?.map(loc => `${String(loc.line)}:${String(loc.column)}`)
+            .join(', ') ?? 'unknown';
+        const pathStr = path?.join('.') ?? 'unknown';
+        console.error(
+          `[GraphQL error]: Message: ${message}, Location: ${locationStr}, Path: ${pathStr}`
+        );
+      }
+    );
+  }
+
+  if (networkError) {
+    console.error(`[Network error]: ${networkError.message}`);
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: `${process.env.NEXT_PUBLIC_INDEXER_MAINNET_URL ?? ''}/v2/graphql`,
+});
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: `${process.env.NEXT_PUBLIC_INDEXER_MAINNET_URL ?? ''}/v2/graphql`,
+    link: from([errorLink, httpLink]),
     cache: new InMemoryCache(),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'cache-and-network',
+        errorPolicy: 'all',
+      },
+      query: {
+        fetchPolicy: 'network-only',
+        errorPolicy: 'all',
+      },
+      mutate: {
+        errorPolicy: 'all',
+      },
+    },
   });
 };
 
