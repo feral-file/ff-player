3798a68cabc0c012aefa30da1c78c7ba938d84f0
Hoang Btmrk <hoang@bitmark.com>
2025-06-25 13:58:49 +0700

    fix: remove logs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  console.log('[RootLayout] Start');
   return (
     <html lang="en">
       <body className={inter.className}>
