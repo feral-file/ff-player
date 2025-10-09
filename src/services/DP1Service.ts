3c1dc46a80d67a6d85570ca0dbfcdb397fdcf891
Hoang Btmrk <hoang@bitmark.com>
2025-10-09 14:23:33 +0700

    remove unused function

--- src/services/DP1Service.ts
+++ src/services/DP1Service.ts
@@ -1,38 +1,9 @@
-import { DP1Call, DP1Item } from '@/models/dp1.model';
-import { convertToTokenID } from '@/utils/indexer';
-import { IndexerService } from './IndexerService';
+import { DP1Call } from '@/models/dp1.model';
 import axios from 'axios';
 import * as Sentry from '@sentry/nextjs';
 import { DEFAULT_PLAYLIST_URL } from '@/constants';
 
 export const DP1Service = {
-  async getItemInfo(item: DP1Item): Promise<{
-    preview: string | null;
-    owner: string | null;
-  } | null> {
-    if (item.provenance?.contract) {
-      const tokenId = convertToTokenID(
-        item.provenance.contract.chain,
-        item.provenance.contract.address,
-        item.provenance.contract.tokenId
-      );
-
-      const token = await IndexerService.queryIndexerToken(tokenId);
-      if (token) {
-        const preview = await IndexerService.getIndexerTokenPreview(token);
-        return {
-          preview,
-          owner: token.owner ?? null,
-        };
-      }
-    }
-
-    return {
-      preview: item.source,
-      owner: null,
-    };
-  },
-
   async getDefaultPlaylist(): Promise<DP1Call | null> {
     try {
       console.log(
